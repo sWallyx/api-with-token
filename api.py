92fb36ec261700fb20b6b4ca1f324413d20d969a
Mikel Sanchez <mikelsmartinez@aol.com>
2020-06-10 13:55:55 +0100

    Run black on main file

--- api.py
+++ api.py
@@ -1,10 +1,15 @@
+""" Main file for the API """
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'thissecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/mikel.sanchez/github/api-with-token/todo.db'
+# TODO: The secret key will end up in as environment variable
+app.config["SECRET_KEY"] = "thissecret"
+# TODO: Find a way to use a relative path for the database
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "sqlite:////Users/mikel.sanchez/github/api-with-token/todo.db"
 
 db = SQLAlchemy(app)
 
@@ -17,11 +22,13 @@ class User(db.Model):
     password = db.Column(db.String(80))
     admin = db.Column(db.Boolean)
 
+
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(50))
     complete = db.Column(db.Boolean)
     user_id = db.Column(db.Integer)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
