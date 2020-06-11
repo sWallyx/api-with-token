54431e7c7b686b5415d23f634b372c44dd1136e1
Mikel Sanchez <mikelsmartinez@aol.com>
2020-06-11 12:54:13 +0100

    Promote user route

--- api.py
+++ api.py
@@ -50,9 +50,21 @@ def get_all_users():
     return jsonify({"users": output})
 
 
-@app.route("/user/<user_id>", methods=["GET"])
-def get_one_user():
-    return ""
+@app.route("/user/<public_id>", methods=["GET"])
+def get_one_user(public_id):
+
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({"message": "No user found!"})
+
+    user_data = {}
+    user_data["public_id"] = user.public_id
+    user_data["name"] = user.name
+    user_data["password"] = user.password
+    user_data["admin"] = user.admin
+
+    return jsonify({"user": user_data})
 
 
 @app.route("/user", methods=["POST"])
@@ -74,12 +86,21 @@ def create_user():
     return jsonify({"message": "New user created"})
 
 
-@app.route("/user/<user_id>", methods=["PUT"])
-def promote_user():
-    return ""
+@app.route("/user/<public_id>", methods=["PUT"])
+def promote_user(public_id):
+
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({"message": "No user found!"})
+
+    user.admin = True
+    db.session.commit()
+
+    return jsonify({"message": "User has been promoted!"})
 
 
-@app.route("/user/<user_id>", methods=["DELETE"])
+@app.route("/user/<public_id>", methods=["DELETE"])
 def delete_user():
     return ""
 
