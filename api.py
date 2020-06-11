b393c9536afbba3f8f066af74e44b11117bb302c
Mikel Sanchez <mikelsmartinez@aol.com>
2020-06-11 13:33:43 +0100

    Add delete route

--- api.py
+++ api.py
@@ -101,8 +101,17 @@ def promote_user(public_id):
 
 
 @app.route("/user/<public_id>", methods=["DELETE"])
-def delete_user():
-    return ""
+def delete_user(public_id):
+
+    user = User.query.filter_by(public_id=public_id).first()
+
+    if not user:
+        return jsonify({"message": "No user found!"})
+
+    db.session.delete(user)
+    db.session.commit()
+    
+    return jsonify({"message":"The user has been deleted"})
 
 
 if __name__ == "__main__":
